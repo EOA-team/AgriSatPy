f3a8744b04cb9fda80e30fb8dc109419860295ec
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-07-14 08:46:33 +0200

    global nodata settings

--- src/agrisatpy/config/sentinel2.py
+++ src/agrisatpy/config/sentinel2.py
@@ -20,3 +20,9 @@ class Sentinel2(BaseModel):
         10.: ['B02', 'B03', 'B04', 'B08'],
         20.: ['B05', 'B06', 'B07', 'B8A', 'B11', 'B12']
     }
+
+    # define nodata values for ESA Sentinel-2 spectral bands (reflectance) and
+    # scene classification layer (SCL)
+    NODATA_REFLECTANCE: int = 64537
+    NODATA_SCL: int = 254
+    
