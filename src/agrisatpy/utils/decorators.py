e72d732cf22b1fdf0afa2e8aaf2a92e6ca273e70
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-07-19 16:10:59 +0200

    make sure processing level is implemented

---
+++ src/agrisatpy/utils/decorators.py
@@ -0,0 +1,31 @@
+'''
+Created on Jul 19, 2021
+
+@author: graflu
+'''
+
+from functools import wraps
+
+from agrisatpy.config import get_settings
+
+
+Settings = get_settings()
+
+
+class ProcessingLevelNotFound(Exception):
+    pass
+
+def check_processing_level(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if len(args) > 0:
+            processing_level = args[1]
+        if kwargs != {}:
+            processing_level = kwargs.get('processing_level', str)
+
+        if not processing_level in Settings.PROCESSING_LEVELS:
+            raise ProcessingLevelNotFound(
+                f'{processing_level} is not part of {Settings.PROCESSING_LEVELS}')
+        return f(*args, **kwargs)
+
+    return wrapper
