e4d68b5cc6b1c0affd37b7de2eb39d781cfb222e
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-08-02 15:40:24 +0200

    small adoptation

--- scripts/01_resample_s2_data.py
+++ scripts/01_resample_s2_data.py
@@ -5,21 +5,21 @@ sample script showing how to start a resampling job for Sentinel-2 data
 from agrisatpy.processing.resampling import exec_parallel
 
 
-# define in and output directory
-# input: directory where the *.SAFE files are located
-raw_data_archive = '/home/graflu/public/Evaluation/Projects/KP0022_DeepField/Sentinel-2/S2_L2A_data/CH/2018'
-
 # define a date range
-year = 2018
-date_start = f'{year}-01-01'
-date_end = '{year}-12-31'
+year = 2020
+date_start = f'{year}-08-01'
+date_end = f'{year}-12-31'
 
 # define a Sentinel2 tile
-tile = 'T32TMT'
+tile = 'T31TGM'
+
+# define in and output directory
+# input: directory where the *.SAFE files are located
+raw_data_archive = f'/home/graflu/public/Evaluation/Projects/KP0022_DeepField/Sentinel-2/S2_L2A_data/CH/{year}'
 
 # output: directory where to store the resampled, band stacked Sentinel2 scenes
 # assuming the default AgriSatPy directory structure
-target_s2_archive = f'/run/media/graflu/ETH-KP-SSD6/SAT/{year}/{tile}'
+target_s2_archive = f'/run/media/graflu/ETH-KP-SSD6/SAT/L2A/{year}/{tile}'
 
 # specify the number of threads
 n_threads = 4
@@ -30,7 +30,7 @@ n_threads = 4
 # use the is_L2A keyword to specify the processing level of the data
 options = {'pixel_division': True,
            'is_L2A': True,
-           'is_mundi': True
+           'is_mundi': False
            }
 
 exec_parallel(raw_data_archive,
