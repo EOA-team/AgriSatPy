8313da4d13764a42f134715daf7fb2704b06956f
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-08-18 10:47:39 +0200

    restructuring imports

--- src/agrisatpy/utils/__init__.py
+++ src/agrisatpy/utils/__init__.py
@@ -1,3 +1,4 @@
 from .stacking import stack_dataframes
 from .archive_search import search_data
-from .sentinel2_merge_blackfill import identify_split_scenes, merge_split_scenes
+from .sentinel2_merge_blackfill import identify_split_scenes
+from .sentinel2_merge_blackfill import merge_split_scenes
