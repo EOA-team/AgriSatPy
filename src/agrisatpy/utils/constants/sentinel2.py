3f0cc428e8519415958ed7366050df0add111644
lukas <lukasvalentin.graf@usys.ethz.ch>
2021-11-25 09:13:47 +0100

    adding native spatial resolutions of S2 bands

--- src/agrisatpy/utils/constants/sentinel2.py
+++ src/agrisatpy/utils/constants/sentinel2.py
@@ -6,6 +6,26 @@ Created on Nov 24, 2021
 
 from enum import Enum
 
+# available processing levels
 class ProcessingLevels(Enum):
     L1C = 'LEVEL1C'
     L2A = 'LEVEL2A'
+
+
+# native spatial resolution of the S2 bands
+band_resolution = {
+    'B01': 60,
+    'B02': 10,
+    'B03': 10,
+    'B04': 10,
+    'B05': 20,
+    'B06': 20,
+    'B07': 20,
+    'B08': 10,
+    'B8A': 10,
+    'B09': 60,
+    'B10': 60,
+    'B11': 20,
+    'B12': 20
+        
+}
