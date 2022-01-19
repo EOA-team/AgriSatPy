22fc54706140330ee82157d63e3ebf617ed9a0a0
lukas <lukasvalentin.graf@usys.ethz.ch>
2022-01-19 16:10:19 +0100

    drafting from_dataframe method (will replace table2raster)
    Former-commit-id: 3c389db35473ccffad6e016c0c163e9efe52224b

--- src/agrisatpy/io/sat_data_creator.py
+++ src/agrisatpy/io/sat_data_creator.py
@@ -3,6 +3,9 @@ The Sat_Data_Creator class allows to create (satellite) image raster data
 with geo-referenzation from numpy.arrays.
 '''
 
+import geopandas as gpd
+
+from typing import List
 from typing import Optional
 
 from agrisatpy.io import SatDataHandler
@@ -12,9 +15,23 @@ from agrisatpy.io import SatDataHandler
 class Sat_Data_Creator(SatDataHandler):
     """
     class for creating new SatDataHandler-like objects.
-
-    :attribute is_bandstack:
-        if False, allows bands to have different spatial resolutions and
-        extends. True by default.
     """
-    pass
+
+    @classmethod
+    def from_dataframe(
+            cls,
+            gdf: gpd.GeoDataFrame,
+            attribute_selection: Optional[List[str]] = None
+        ):
+        """
+        Creates a new handler instance from a ``GeoDataFrame``
+
+        :param gdf:
+            ``GeoDataFrame`` with Point records to convert into a
+            new handler instance
+        :param attribute_selection:
+            attributes (columns) of the ``GeoDataFrame`` to convert
+            into raster bands. Each attribute is converted into a single
+            band.
+        """
+        pass
