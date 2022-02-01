681395a9b01b19887a5b9a931e73095aa50dcce5
lukas <lukasvalentin.graf@usys.ethz.ch>
2022-02-01 11:22:19 +0100

    moving upsampling function to arrays where it's more logical to store
    Former-commit-id: 61d529f2530eeef192592bce17e573db4596685b

--- src/agrisatpy/utils/arrays.py
+++ src/agrisatpy/utils/arrays.py
@@ -4,11 +4,13 @@ Created on Dec 10, 2021
 @author: Lukas Graf
 '''
 
+import itertools
 import numpy as np
 
 from numpy.ma.core import MaskedArray
 from typing import Union
 from typing import Optional
+
 from agrisatpy.utils.exceptions import InputError
 
 
@@ -27,7 +29,7 @@ def count_valid(
     :param no_data_value:
         no data value indicating invalid array elements. Default set to zero.
         Ignored if ``in_array`` is a ``MaskedArray``
-    :return:
+    :returns:
         number of invalid array elements.
     """
 
@@ -42,3 +44,47 @@ def count_valid(
 
     # check if array is np.array or np.ndarray
     return (in_array != no_data_value).sum()
+
+def upsample_array(
+        in_array: np.array,
+        scaling_factor: int,
+    ) -> np.array:
+    """
+    takes a 2-dimensional input array (i.e., image matrix) and splits every
+    array cell (i.e., pixel) into X smaller ones having all the same value
+    as the "super" cell they belong to, where X is the scaling factor (X>=1).
+    This way the input image matrix gets a higher spatial resolution without
+    changing any of the original pixel values.
+
+    The value of the scaling_factor determines the spatial resolution of the output.
+    If scaling_factor = 1 then the input and the output array are the same.
+    If scaling_factor = 2 then the output array has a spatial resolution two times
+    higher then the input (e.g. from 20 to 10 m), and so on.
+
+    :param array_in:
+        2-d array (image matrix)
+    :param scaling_factor:
+        factor for increasing spatial resolution. Must be greater than/ equal to 1
+    :returns:
+        upsampled array with pixel values in target spatial resolution
+    """
+
+    # check inputs
+    if scaling_factor < 1:
+        raise ValueError('scaling_factor must be greater/equal 1')
+
+    # define output image matrix array bounds
+    shape_out = (in_array.shape[0]*scaling_factor,
+                 in_array.shape[1]*scaling_factor)
+    out_array = np.zeros(shape_out, dtype = in_array.dtype)
+
+    # increase resolution using itertools by repeating pixel values
+    # scaling_factor times
+    counter = 0
+    for row in range(in_array.shape[0]):
+        column = in_array[row, :]
+        out_array[counter:counter+scaling_factor,:] = list(
+            itertools.chain.from_iterable(
+                itertools.repeat(x, scaling_factor) for x in column))
+        counter += scaling_factor
+    return out_array
